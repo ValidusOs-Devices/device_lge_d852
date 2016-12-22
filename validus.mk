15b3778d1d2bcab1848c8497976e005d56e56bdd
oadam11 <ovadia.adam@gmail.com>
2016-12-22 22:19:55 +0100

    val pls

--- validus.mk
+++ validus.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/d852/full_d852.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := tesla_d852
+PRODUCT_NAME := validus_d852
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
